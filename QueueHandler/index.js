ddf2933e3a3d8ca43733a04b8f41ba371cd3cb37
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-03-29 07:30:39 +0100

    Adds timeStamp

--- QueueHandler/index.js
+++ QueueHandler/index.js
@@ -7,7 +7,7 @@ module.exports = async function (context, mySbMsg) {
     context.log(`azf-shared-lighthouse - ${mySbMsg.eventSourceId} - url - ${pageUrl} - lighthouse - start`)
     let result = await checkLighthouse(context, pageUrl)
     context.log(`azf-shared-lighthouse - ${mySbMsg.eventSourceId} - url - ${pageUrl} - lighthouse - success`)
-    context.bindings.mySbQueue = Object.assign({}, mySbMsg, { domain: 'webquality', category: 'lighthouse', date: new Date(), result: result })
+    context.bindings.mySbQueue = Object.assign({}, mySbMsg, { domain: 'webquality', category: 'lighthouse', date: new Date(), timeStamp: new Date().getTime(), result: result })
     context.log(`azf-shared-lighthouse - ${mySbMsg.eventSourceId} - url - ${pageUrl} - finished`)
   } catch (error) {
     context.log.error(`azf-shared-lighthouse - ${mySbMsg.eventSourceId} - url - ${pageUrl} - ${error}`)
